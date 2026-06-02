bc89b8f59aee1e506d9065a3dd7e3b9dab110d45
d4rkc0d3r <1735522+d4rkc0d3r@users.noreply.github.com>
2026-06-02 16:50:00 +0200

    add pull and build bat scripts

---
+++ build.bat
@@ -0,0 +1,9 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+cd llama.cpp-latest
+cmake -B build -G Ninja ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DGGML_CUDA=ON ^
+  -DGGML_CUDA_FA_ALL_QUANTS=ON ^
+  -DGGML_NATIVE=ON ^
+  -DCMAKE_CUDA_ARCHITECTURES=native
+cmake --build build -j
---
+++ pull_from_github.bat
@@ -0,0 +1,2 @@
+rmdir /s /q llama.cpp-latest
+git clone https://github.com/ggml-org/llama.cpp.git llama.cpp-latest
