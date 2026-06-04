cc39b4c11737ec0f8b6f0a33300d13e7582d8704
d4rkc0d3r <1735522+d4rkc0d3r@users.noreply.github.com>
2026-06-04 16:06:19 +0200

    Update pull_from_github.bat

--- pull_from_github.bat
+++ pull_from_github.bat
@@ -1,2 +1,5 @@
-rmdir /s /q llama.cpp-latest
-git clone https://github.com/ggml-org/llama.cpp.git llama.cpp-latest
+:: rmdir /s /q llama.cpp-latest
+:: git clone https://github.com/ggml-org/llama.cpp.git llama.cpp-latest
+cd llama.cpp-latest
+git fetch
+git pull
